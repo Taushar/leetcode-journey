70a77eac02c752cf32f1c5f1086ce5ec18cab393
Taushar lekhraj Maheshwari <141675858+Taushar@users.noreply.github.com>
2026-09-24 22:23:16 +0100

    Time: 911 ms (93.95%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+SELECT Department,Employee,salary
+FROM(
+SELECT d.name AS Department,e.name AS Employee,e.salary,
+DENSE_RANK()OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC)AS rnk
+FROM Employee e
+JOIN Department d ON e.departmentId=d.id
+)t
+WHERE rnk<=3;
